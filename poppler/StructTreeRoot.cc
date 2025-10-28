47555ae68f3c42a501ac3262383d9e7ac85d3605
poc user <poc-user@root.io>
2025-10-28 20:34:53 +0530

    hooks disabled

--- poppler/StructTreeRoot.cc
+++ poppler/StructTreeRoot.cc
@@ -137,6 +137,10 @@ void StructTreeRoot::parseNumberTreeNode(Dict *node)
                 }
                 int keyVal = key.getInt();
                 std::vector<Parent> &vec = parentTree[keyVal];
+                if (!vec.empty()) {
+                    error(errSyntaxError, -1, "Nums item at position {0:d} is a duplicate entry for key {1:d}", i, keyVal);
+                    continue;
+                }
 
                 Object valueArray = nums.arrayGet(i + 1);
                 if (valueArray.isArray()) {
